cdeccd90c129b72c23ebda04ecef692055ee340b
Raihan R <raihanryuichi@gmail.com>
2022-05-16 12:29:44 +0700

    Add more root page sample 2

---
+++ root-lit/src/App.js
@@ -0,0 +1,50 @@
+import { LitElement, adoptStyles, unsafeCSS, html, css } from 'lit'
+
+import LitNavbar from 'app_lit/Navbar'
+import ReactHeader from 'app_react/Header'
+import VanillaTitle from 'app_vanilla/Title'
+import SvelteSidebar from 'app_svelte/Sidebar'
+import VueFooter from 'app_vue/Footer'
+
+export class App extends LitElement {
+    static styles = css`
+        div {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    `
+
+    render() {
+        return html`
+            <navbar-lit></navbar-lit>
+            <div id="header-react"></div>
+            <div id="title-vanilla"></div>
+            <div>
+                Lit-App !
+            </div>
+            <div id="sidebar-svelte"></div>
+            <div id="footer-vue"></div>
+        `
+    }
+
+    firstUpdated() {
+        const mountHeader = this.renderRoot.querySelector('#header-react')
+        const mountTitle = this.renderRoot.querySelector('#title-vanilla')
+        const mountSidebar = this.renderRoot.querySelector('#sidebar-svelte')
+        const mountFooter = this.renderRoot.querySelector('#footer-vue')
+    
+        LitNavbar('navbar-lit')
+        ReactHeader(mountHeader)
+        VanillaTitle(mountTitle)
+        SvelteSidebar(mountSidebar)
+        VueFooter(mountFooter)
+
+        const styleList = []
+        document.querySelectorAll('style').forEach(e => {
+            styleList.push(css`${unsafeCSS(e.childNodes[0].nodeValue)}`)
+        })
+
+        adoptStyles(this.renderRoot, styleList)
+
+    }
+}
+customElements.define('root-lit', App)
---
+++ root-lit/webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const { dependencies } = require("./package.json");
+
+module.exports = {
+  output: {
+    publicPath: "http://localhost:3034/",
+  },
+
+  resolve: {
+    extensions: [".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 3034,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "root_lit",
+      filename: "remoteEntry.js",
+      remotes: {
+        'app_vanilla': 'app_vanilla@http://localhost:3000/remoteEntry.js',
+        'app_vue': 'app_vue@http://localhost:3001/remoteEntry.js',
+        'app_react': 'app_react@http://localhost:3002/remoteEntry.js',
+        'app_svelte': 'app_svelte@http://localhost:3003/remoteEntry.js',
+        'app_lit': 'app_lit@http://localhost:3004/remoteEntry.js'
+      },
+      exposes: {},
+      shared: {
+        ...dependencies,
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
